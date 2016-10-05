feed2f3c7c1d82c3bfba21425d0f94f7bcc131c4
anoopmundathan <anoop.mundathan@gmail.com>
2016-10-05 05:20:27 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+http.createServer((req, res) => {
+	res.writeHead(200, {'Content-Type':'text/html'});
+	res.write('<h1>Hello World</h1>');
+	res.end();
+}).listen(3000);
+
+console.log('server running at port 3000')
